596b9f88fd0d82e7cda3104aabb9617b1a040627
조민수 <chominsu10@gmail.com>
2022-04-05 19:46:38 +0900

    [ADD] login api

--- front/src/pages/LoginPage/LoginPage.tsx
+++ front/src/pages/LoginPage/LoginPage.tsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { LoginPageContainer, LoginPageContent } from "./LoginPageStyle";
 import logo from "assets/png/jobl_logo.png";
 import { AuthInput } from "components/common/InputStyle";
 import { AuthButton } from "components/common/ButtonStyle";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const [id, setId] = useState("");
+  const [pwd, setPwd] = useState("");
+  const navigate = useNavigate();
+
   return (
     <LoginPageContainer>
       <LoginPageContent>
         <img src={logo} alt="" />
-        <AuthInput placeholder="아이디" />
-        <AuthInput placeholder="비밀번호" type="password" />
+        <AuthInput
+          value={id}
+          onChange={e => setId(e.target.value)}
+          placeholder="아이디"
+        />
+        <AuthInput
+          value={pwd}
+          onChange={e => setPwd(e.target.value)}
+          placeholder="비밀번호"
+          type="password"
+        />
         <div>
-          <AuthButton>로그인</AuthButton>
+          <AuthButton
+            onClick={async () => {
+              const result = await axios.post("http://127.0.0.1:5000/login", {
+                id,
+                password: pwd,
+              });
+              if (result.status == 200) {
+                navigate("/");
+                localStorage.setItem("AccessToken", result.data.data);
+              }
+            }}
+          >
+            로그인
+          </AuthButton>
           <AuthButton>회원가입 하러 가기</AuthButton>
         </div>
       </LoginPageContent>
