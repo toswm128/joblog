a5a0764ad0c2a15fba3b127cc7cbc1011d984743
조민수 <chominsu10@gmail.com>
2022-05-31 12:10:21 +0900

    [UPDATE] wait all components ready on MyPage
    Show loading when fetching all queries in myPage.

--- front/src/pages/Blog/MyPage/MyPage.tsx
+++ front/src/pages/Blog/MyPage/MyPage.tsx
@@ -1,7 +1,11 @@
 import My from "components/Blog/My";
+import Loader from "components/common/Loader";
+import { useIsFetching, useQueryClient } from "react-query";
 
 const MyPage = () => {
-  return <My />;
+  const isFetching = useIsFetching();
+  console.log(isFetching);
+  return <>{isFetching ? <Loader /> : <My />}</>;
 };
 
 export default MyPage;
