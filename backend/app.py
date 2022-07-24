a3bec0eb74cd58dd2e7dc2980cdc259d0f00bded
조민수 <63090043+toswm128@users.noreply.github.com>
2022-07-24 14:39:14 +0900

    [UPDATE] relative path
    될대로 되라식

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ def create_app():
     app = Flask(__name__)
     app.config['JSON_AS_ASCII'] = False
     CORS(app, resources={r"*": {"origins": "*"}})
-    app.config["IMAGE_UPLOADS"] = "/home/images"
+    app.config["IMAGE_UPLOADS"] = "../../../../../../home/images"
 
 
 
