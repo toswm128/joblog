8623099f28bf9c6c2958e6422a65bcd95128330a
조민수 <63090043+toswm128@users.noreply.github.com>
2022-08-26 12:18:15 +0900

    [ADD] isMyBoard

--- front/src/components/Blog/Board/Board.tsx
+++ front/src/components/Blog/Board/Board.tsx
@@ -9,19 +9,30 @@ import Divider from "components/common/Divider";
 import BoardHeader from "./BoardHeader";
 import NewBlogs from "components/common/NewBlogs";
 import { anotherBreakPoints } from "assets/breakpoints/breakpoints";
+import useAuthAPI from "hooks/API/useAuthAPI";
+import { useEffect, useState } from "react";
 
 interface IBoard {
   idx: string;
 }
 
 const Board = ({ idx }: IBoard) => {
+  const [isMyBoard, setIsMyBoard] = useState(false);
   const { getBoard, getBlog } = useBlogAPI();
+  const { GetUser } = useAuthAPI();
+  const { data: { data: info } = {} } = useQuery("myInfo", GetUser);
 
   const { isLoading, data: { data: board } = {} } = useQuery(
     `board/${idx}`,
     () => getBoard(idx),
     {}
   );
+
+  useEffect(() => {
+    board?.blog?.userIdx ??
+      info?.idx ??
+      setIsMyBoard(board?.blog?.userIdx === info?.idx);
+  }, [board?.blog, info?.idx]);
 
   return (
     <BoardContainer>
